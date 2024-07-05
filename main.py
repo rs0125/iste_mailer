e4374da22b8fbd399dc2ec1113a6bba86f2047e1
mirshadalis <coding.irshadali@gmail.com>
2024-07-05 13:55:17 +0530

    Fixed the bot

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+import json
 import datetime
-from static import data
+import schedule
+import time
 
-#basic function for checking if tommorow's date is in our birthday list
-def finder(birthday_list):
-    tommorow = datetime.datetime.today() + datetime.timedelta(days=1)
-    for i in birthday_list:
-        if int(i['birthday'][8:10]) == int(tommorow.day) and int(i['birthday'][5:7]) == int(tommorow.month):
-            print(i['name'] , "has a birthday tommorow")
+load_dotenv()
+
+months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+
+def check_bday(obj):
+    name = obj["name"]
+    phone = int(obj["phone_number"])
+    CurrentDate = datetime.datetime.now().strftime('%Y-%m-%d')
+    obj_date = obj["birthday"]
+    obj_date = obj_date.split("-")
+    CurrentDate = CurrentDate.split("-")
+    if(int(obj_date[1]) == int(CurrentDate[1])):
+        if (int(CurrentDate[2]) == (int(obj_date[2]) - 1)):
+            day = obj_date[2]
+            month = months[int(obj_date[1])]
+            wish = f"Subject: Birthday Wish \n\nIts {name}'s birthday on {day} of {month}, Phone Number: +91{phone}"
+            return wish
+    return None
 
 
-# testing here
-finder(data)
+if __name__ == "__main__":
+    email = os.environ.get('MY_EMAIL')
+    rev = os.environ.get('VED_EMAIL')
+    token = os.environ.get('TOKEN')
+    subject = "Wish Reminder"
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(email, token)
+            print("Successfully Logged in")
+            try:
+                with open('iste2024.json', 'r') as f:
+                    data = json.load(f)
+                print("Loaded Data")
+                for person in data:
+                    wish = check_bday(person)
+                    if (wish != None):
+                        server.sendmail(email, rev, wish)
+                        print(f"Sent Email : {wish}")
+            except Exception as e:
+                print(f"Error Importing JSON File: {e}")
+    except Exception as e:
+        print(f"Server Unavailable: {e}")
